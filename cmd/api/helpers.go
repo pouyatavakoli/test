3a8d2c6d5bfdc48ae996f6a22cb4104e9bb47c2d
Pouya Tavakoli <69327223+pouyatavakoli@users.noreply.github.com>
2026-03-13 18:48:36 +0330

    Create helpers.go

---
+++ cmd/api/helpers.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+	return id, nil
+}
+
+func (app *application) readPageParameter(r *http.Request) (int64, error) {
+	pageStr := r.URL.Query().Get("page")
+	if pageStr == "" {
+		return 1, nil
+	}
+
+	p, err := strconv.ParseInt(pageStr, 10, 64)
+	if err != nil || p < 1 {
+		return 0, fmt.Errorf("invalid page number")
+	}
+
+	return p, nil
+}
+
+type envelope map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
+}
